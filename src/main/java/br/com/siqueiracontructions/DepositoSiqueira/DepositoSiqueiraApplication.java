74d8248c58eabeec5d656e7e47863264fb8af1ba
Alexandre Júnior <devjuniorss@gmail.com>
2024-03-08 10:37:27 -0300

    first commit upando base do projeto do spring,  revisar dependencias

---
+++ src/main/java/br/com/siqueiracontructions/DepositoSiqueira/DepositoSiqueiraApplication.java
@@ -0,0 +1,13 @@
+package br.com.siqueiracontructions.DepositoSiqueira;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DepositoSiqueiraApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DepositoSiqueiraApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/siqueiracontructions/DepositoSiqueira/DepositoSiqueiraApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.siqueiracontructions.DepositoSiqueira;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DepositoSiqueiraApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
